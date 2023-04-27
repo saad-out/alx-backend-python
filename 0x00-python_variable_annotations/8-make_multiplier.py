65cc78efc5fdf062ca9887a6cabdf753ef74883f
saad-out <outsaad03@gmail.com>
2023-04-27 12:04:48 +0100

    function (callable)

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+This module contains a function that returns a function that
+multiplies a float by multiplier
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Returns a function that multiplies a float by multiplier
+
+    Args:
+        multiplier (float): A float number
+
+    Returns:
+        Callable[[float], float]: A function that multiplies
+                                a float by multiplier
+    """
+    def multiply_by_multiplier(number: float):
+        """
+        Multiplies a float by multiplier
+
+        Args:
+            number (float): A float number
+
+        Returns:
+            float: The product of multiplier and number
+        """
+        return float(multiplier * number)
+
+    return multiply_by_multiplier
