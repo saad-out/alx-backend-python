0af6f0969ef36998bc9c172c0e65f27b703ed92e
saad-out <outsaad03@gmail.com>
2023-04-27 11:13:08 +0100

    basic str annotations

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This module contains a function to return the concatinated string of 2 strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Returns the concatinated string of 2 strings
+
+    Args:
+        str1 (str): first string
+        str2 (str): second string
+
+    Returns:
+        str: concatinated string of str1 and str2
+    """
+    return str1 + str2
