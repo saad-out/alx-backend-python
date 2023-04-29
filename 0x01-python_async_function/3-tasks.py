82a779d2a4b31de3a97b31ddb3e980ceaa170481
saad-out <outsaad03@gmail.com>
2023-04-29 12:09:05 +0100

    create task

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+This module contains the function task_wait_random that takes
+an integer max_delay and returns a asyncio.Task.
+"""
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+    Return an asyncio.Task
+
+    Args:
+        max_delay (int): max random delay
+
+    Returns:
+        asyncio.Task: asyncio.Task
+    """
+    return asyncio.create_task(wait_random(max_delay))
