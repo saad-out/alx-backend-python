acdb4af16254aa171397fbf3e5562e4feb127dff
saad-out <outsaad03@gmail.com>
2023-05-20 13:09:10 +0100

    parameterized with exception

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -29,6 +29,21 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         self.assertEqual(access_nested_map(nested_map, path), expected_output)
 
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(
+                               self,
+                               nested_map: Mapping,
+                               path: Sequence,
+                               ) -> None:
+        """
+        Test access_nested_map function with exception
+        """
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(nested_map, path)
+
 
 if __name__ == '__main__':
     unittest.main()
