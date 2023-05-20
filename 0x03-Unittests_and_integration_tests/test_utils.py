4f51bbe4589646330f2cff700b6d03e53c1f2891
saad-out <outsaad03@gmail.com>
2023-05-20 16:53:47 +0100

    mock HTTP calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,10 +3,17 @@
 This module contains the unit tests for utils.py
 """
 from parameterized import parameterized
-from typing import Mapping, Sequence, Any
+from typing import (
+    Mapping,
+    Sequence,
+    Dict,
+    Any
+)
 import unittest
+from unittest.mock import patch
 
-from utils import access_nested_map
+import utils
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -43,6 +50,28 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         with self.assertRaises(KeyError) as e:
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """
+    Class for testing get_json function
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(
+                      self,
+                      test_url: str,
+                      test_payload: Dict[str, bool]
+                      ) -> None:
+        """
+        Test get_json function
+        """
+        with patch("utils.requests.get") as mocked_get:
+            mocked_get.return_value.json.return_value = test_payload
+            self.assertEqual(get_json(test_url), test_payload)
+            mocked_get.assert_called_once_with(test_url)
 
 
 if __name__ == '__main__':
